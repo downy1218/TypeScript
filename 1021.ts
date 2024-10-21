307dc1573144848fef62c6f3e1b392a1635b2fb5
Daeuni <ekdmsjj@gmail.com>
2024-10-21 23:57:14 +0900

    1021

--- 1021.ts
+++ 1021.ts
@@ -1,7 +1,84 @@
 //cmd켜라
 
 
-`타입이 아직 하나로 정해지지 않았을 때 narrowing
+`타입이 아직 하나로 정해지지 않았을 때 (유니온 타입일 때)  narrowing문법을 써서 타입을 하나로 확정해줌 
 대표적인 방법은 if(type of x === 'string')
 또 다른 방법으로는 as x 가 있음 
-변수[0] = x as number    ==> assertion 문법`
+변수[0] = x as number    ==> assertion 문법  좋다고 막 쓰는 거 아니고 유니온 타입일 때 사용하는 것
+as 문법보다는 if문을 사용하는 게 훨씬 안전함 `
+
+// function cleaning(a:(string|number)[]){
+//     for (let i=0; i<a.length; i++){
+//         if(typeof a[i] === 'string'){
+//             Number(a[i])
+//             return a
+//         }else{
+//             return a
+//         }
+//     }
+// }
+// console.log(cleaning([1,2,'3']));
+
+
+
+
+function clean(x:(string|number)[]){
+    return(
+        x.map((item)=>{
+            if(typeof item === 'string'){
+                let changed = Number(item)
+                return(changed);       
+            }else{
+                return item
+            }
+        })
+    )
+};
+console.log(clean([1,2,'3'])); 
+
+
+
+function ssam(x:{sub:string|string[]}){
+    if(typeof x.sub === 'string'){
+        return x.sub
+    }
+    else if(Array.isArray(x.sub)){
+        let changed = x.sub.pop()
+        return changed
+    }
+    else{
+        return 'none'
+    }
+};
+//else if (Array.isArray(x.sub) ){
+    //return x.sub[x.sub.length - 1]
+console.log({x:'math'});
+console.log({x:['math','music']});
+
+
+
+
+// 타입을 변수에 담아 쓰기 type alias
+// 작명 시 대문자로 작명
+type AnimalType = number|string|boolean;
+let 동물:AnimalType = '사자';
+
+type Girlfriend = {
+    readonly name : string,
+  }
+  
+  let 여친 :Girlfriend = {
+    name : '엠버'
+  }
+  
+  //여친.name = '유라' //readonly라서 에러남
+
+
+// 타입 합치기 가능
+type Name = string;
+type Age = number;
+type NewOne = Name | Age; 
+
+
+
+
--- dist/1021.js
+++ dist/1021.js
@@ -1,2 +1,47 @@
 //cmd켜라
-"\uD0C0\uC785\uC774 \uC544\uC9C1 \uD558\uB098\uB85C \uC815\uD574\uC9C0\uC9C0 \uC54A\uC558\uC744 \uB54C narrowing\n\uB300\uD45C\uC801\uC778 \uBC29\uBC95\uC740 if(type of x === 'string')\n\uB610 \uB2E4\uB978 \uBC29\uBC95\uC73C\uB85C\uB294 as x \uAC00 \uC788\uC74C \n\uBCC0\uC218[0] = x as number    ==> assertion \uBB38\uBC95";
+"\uD0C0\uC785\uC774 \uC544\uC9C1 \uD558\uB098\uB85C \uC815\uD574\uC9C0\uC9C0 \uC54A\uC558\uC744 \uB54C (\uC720\uB2C8\uC628 \uD0C0\uC785\uC77C \uB54C)  narrowing\uBB38\uBC95\uC744 \uC368\uC11C \uD0C0\uC785\uC744 \uD558\uB098\uB85C \uD655\uC815\uD574\uC90C \n\uB300\uD45C\uC801\uC778 \uBC29\uBC95\uC740 if(type of x === 'string')\n\uB610 \uB2E4\uB978 \uBC29\uBC95\uC73C\uB85C\uB294 as x \uAC00 \uC788\uC74C \n\uBCC0\uC218[0] = x as number    ==> assertion \uBB38\uBC95  \uC88B\uB2E4\uACE0 \uB9C9 \uC4F0\uB294 \uAC70 \uC544\uB2C8\uACE0 \uC720\uB2C8\uC628 \uD0C0\uC785\uC77C \uB54C \uC0AC\uC6A9\uD558\uB294 \uAC83\nas \uBB38\uBC95\uBCF4\uB2E4\uB294 if\uBB38\uC744 \uC0AC\uC6A9\uD558\uB294 \uAC8C \uD6E8\uC52C \uC548\uC804\uD568 ";
+// function cleaning(a:(string|number)[]){
+//     for (let i=0; i<a.length; i++){
+//         if(typeof a[i] === 'string'){
+//             Number(a[i])
+//             return a
+//         }else{
+//             return a
+//         }
+//     }
+// }
+// console.log(cleaning([1,2,'3']));
+function clean(x) {
+    return (x.map(function (item) {
+        if (typeof item === 'string') {
+            var changed = Number(item);
+            return (changed);
+        }
+        else {
+            return item;
+        }
+    }));
+}
+;
+console.log(clean([1, 2, '3']));
+function ssam(x) {
+    if (typeof x.sub === 'string') {
+        return x.sub;
+    }
+    else if (Array.isArray(x.sub)) {
+        var changed = x.sub.pop();
+        return changed;
+    }
+    else {
+        return 'none';
+    }
+}
+;
+//else if (Array.isArray(x.sub) ){
+//return x.sub[x.sub.length - 1]
+console.log({ x: 'math' });
+console.log({ x: ['math', 'music'] });
+var 동물 = '사자';
+var 여친 = {
+    name: '엠버'
+};
